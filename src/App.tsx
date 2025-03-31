bd55df4bfa163e3f6fbe6263f2686a8a37c4a374
e.covello <emanuele.covello@gmail.com>
2025-03-31 16:57:21 +0200

    replace h1 with H1 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
 import Style from './App.module.css';
 import SearchFund from './components/funds/search-fund/SearchFund';
+import { H1 } from './components/base/headings/Headings';
 const App: FC = () => {
   return (
     <div className={Style.App}>
       <header>
         <div className={Style.Header}>
           <div className={Style.HeaderContent}>
-            <h1>ETF Screener</h1>
+            <H1>ETF Screener</H1>
             <SearchFund />
           </div>
         </div>
