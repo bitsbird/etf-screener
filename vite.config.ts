af938fc0b498ff20757bfe6d546cfeb68fc5eddf
e.covello <emanuele.covello@gmail.com>
2025-04-04 16:15:50 +0200

    import svg as react component

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import postCssGlobalData from '@csstools/postcss-global-data';
 import postCssCunstomMedia from 'postcss-custom-media';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    svgr({
+      svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
+      include: '**/*.svg',
+    }),
+    react(),
+  ],
   server: {
     port: 8080,
   },
