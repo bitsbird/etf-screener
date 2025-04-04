bdcbcd5d0423178f741d67b7689bcb9ad05d33f9
e.covello <emanuele.covello@gmail.com>
2025-04-04 16:53:25 +0200

    add fund to porfolio design

--- src/components/funds/fund-list/FundList.tsx
+++ src/components/funds/fund-list/FundList.tsx
@@ -4,21 +4,27 @@ import { useContext } from 'react';
 import { FundContext } from '../FundContext';
 import { H2 } from '../../base/headings/Headings';
 import DataProp from '../../base/data-prop/DataProp';
+import WalletButton from '../../wallet/WalletButton';
 const FundList: FC = () => {
   const { fundList } = useContext(FundContext);
   return (
     <div className={Styles.FundList}>
       {fundList.map((fund) => (
         <div key={fund.isin} className={Styles.Item}>
-          <div className={Styles.Header}>
-            <H2>{fund.name}</H2>
+          <div className={Styles.ItemData}>
+            <div className={Styles.Header}>
+              <H2>{fund.name}</H2>
+            </div>
+            <DataProp label="ISIN" value={fund.isin} />
+            <DataProp label="Benchmark Index" value={fund.benchmark_index} />
+            <DataProp
+              label="Expense Ratio"
+              value={fund.expense_ratio ? String(fund.expense_ratio) : ''}
+            />
           </div>
-          <DataProp label="ISIN" value={fund.isin} />
-          <DataProp label="Benchmark Index" value={fund.benchmark_index} />
-          <DataProp
-            label="Expense Ratio"
-            value={fund.expense_ratio ? String(fund.expense_ratio) : ''}
-          />
+          <div>
+            <WalletButton action="add" />
+          </div>
         </div>
       ))}
     </div>
--- src/components/wallet/WalletButton.tsx
+++ src/components/wallet/WalletButton.tsx
@@ -6,12 +6,22 @@ import Styles from './WalletButton.module.css';
 interface Props {
   action?: 'add' | 'remove';
 }
+const actionConfig = {
+  default: { title: 'Portfilio', text: '' },
+  add: { title: 'Add to portfolio', text: '+' },
+  remove: { title: 'Remove', text: 'x' },
+};
 
 const WalletButton: FC<Props> = ({ action }) => {
-  const actionText = action === 'add' ? '+' : action === 'remove' ? 'x' : '';
+  let actionTitle = actionConfig.default.title;
+  let actionText = actionConfig.default.text;
+  if (action) {
+    actionTitle = actionConfig[action].title;
+    actionText = actionConfig[action].text;
+  }
   return (
     <div className={Styles.WalletButton}>
-      <BaseButton type="transparent" title="portfolio">
+      <BaseButton type="transparent" title={actionTitle}>
         <WalletSvg />
         <div className={Styles.Action}>{actionText}</div>
       </BaseButton>
